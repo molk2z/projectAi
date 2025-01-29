9874030bd1ec657711e51a24c7708244fec3dd87
زكريا السلطان | zakarya Al_sultan <zico2001s@yahoo.com>
2025-01-29 22:08:05 +0300

    update mpdle page here we divide data to x,y to trin

--- model.py
+++ model.py
@@ -28,3 +28,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 import joblib
+
+
+# تقسيم البيانات
+X = dataset['text']
+y = dataset['label']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# تحويل النصوص إلى ميزات عددية
+vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
