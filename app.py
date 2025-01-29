4b75c299cbedb62a252871c670e5c866aa319896
زكريا السلطان | zakarya Al_sultan <zico2001s@yahoo.com>
2025-01-29 22:11:54 +0300

    update page app.py definition app

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask, render_template, request
 import joblib
 import numpy as np
+
+
+# تعريف التطبيق Flask
+app = Flask(__name__)
+
+# تحميل النموذج المدرب مسبقًا
+model = joblib.load('news_model.pkl')
+vectorizer = joblib.load('vectorizer.pkl')
